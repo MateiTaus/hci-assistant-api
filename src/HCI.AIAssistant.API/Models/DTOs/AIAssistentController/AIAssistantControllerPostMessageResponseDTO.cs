ac36758466955fdccf661d765b247a0c92f4067f
MateiTaus <150827765+MateiTaus@users.noreply.github.com>
2025-11-14 14:42:01 +0200

    first

---
+++ src/HCI.AIAssistant.API/Models/DTOs/AIAssistentController/AIAssistantControllerPostMessageResponseDTO.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCI.AIAssistant.API.Models.DTOs.AIAssistentController;
+ public class AIAssistantControllerPostMessageResponseDTO
+{
+    public string? TextMessage { get; set; }    
+}
